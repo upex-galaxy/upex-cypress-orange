15754517224852dcbab44722990d38161f662887
Ely <elyermad@gmail.com>
2022-09-10 02:03:53 -0300

    Cypress10.7 run PASSED

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
-	projectId: 'zm1f4f',
+	projectId: '31yjoc',
 	watchForFileChanges: false,
 	chromeWebSecurity: false,
 	// reporter: 'mochawesome',

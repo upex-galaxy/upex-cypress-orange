2bec3de93a5bd68dcc48ef64be0ed49ab8d89aee
Delia <delia_cardenas_30@hotmail.com>
2022-10-24 19:34:04 -0300

    story

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,6 +7,7 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
+import cypress from 'cypress';
 import 'cypress-file-upload';
 require('@4tw/cypress-drag-drop')
 
@@ -26,3 +27,24 @@ require('@4tw/cypress-drag-drop')
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+
+Cypress.Commands.add("LoginOrange", () =>
+{
+    cy.fixture("DOM/PIM/EditarPerfilDeEmpleado.Page.json").then((the) =>
+    {
+        cy.visit(the.url)// visita la url
+        cy.url().should("contain", "orangehrm")
+
+        cy.get(the.input.username).type(the.data.username)//Obtener input Username y escribir el usuario
+        .should("have.value",the.data.username) // Debería contener el el valor de usuario
+
+        cy.get(the.input.password).type(the.data.password) //Obtener input Password y escribir el contraseña
+        .should("have.value", the.data.password) // Debería contener el el valor de la contraseña
+
+        cy.get(the.input.buttonLogin).click() // Contiene Login, hacer click
+    })
+})
+
+

68e85b4ddcac5c27bb3615d4030639a861524f07
Rafael Mejia <rafaeldj4@gmail.com>
2022-10-25 13:49:49 -0400

    Error en los pasos duplicado

---
+++ cypress/e2e/cucumber-test/Suites/GX2-146/accountForgetPassword.js
@@ -0,0 +1,72 @@
+import { Given, When, And, Then } from "@badeball/cypress-cucumber-preprocessor";
+import ForgetPasswordPage from "../../../../support/pages/OrangeHRM/ForgetPasswordPage";
+
+Given("el usuario esta en el endpoint {string}", (endpoint) => {
+        ForgetPasswordPage.visit()
+        cy.url().should("contain",endpoint) 
+})
+
+describe("US GX2-146 | TS GX2-147 | TC1: Validar recuperar la contraseña de usuario con cuenta previamente creada", () => {
+    
+    Given("el usuario tiene una cuenta previamente creada", () => {
+        cy.log("Cuenta creada")
+    })
+
+    When("el usuario ingresa el username {string} en el input username y hace click en el boton Reset Password", (validUsername) => {
+        ForgetPasswordPage.typeUsername(validUsername)
+        ForgetPasswordPage.elements.resetPassword_button().click()
+    })
+
+    Then("debe aparecer el mensaje {string}", (resetPasswordsuccessfullyMessage) => {
+        ForgetPasswordPage.elements.successfullResetPasswordMessage().should("be.visible").and("have.text",resetPasswordsuccessfullyMessage)
+    })
+
+    And("debe indicar el siguente texto: {string}", (succesfullEmailMessage) => {
+        cy.log(succesfullEmailMessage)
+    })
+
+})
+
+// * TC2
+describe("US GX2-146 | TS GX2-147 | TC2:  Validar que no permita recuperar la contraseña de usuario previamente no creado", () => {
+    
+    Given("el usuario NO tiene una cuenta previamente creada", () => {
+        cy.log("Usuario con cuenta no creada")
+    })
+
+    When("el usuario ingresa el username {string} en el input username y hace click en el boton Reset Password", (invalidUsername) => {
+        ForgetPasswordPage.typeUsername(invalidUsername)
+        ForgetPasswordPage.elements.resetPassword_button().click()
+    })
+
+    Then("No debe aparecer el mensaje {string}", (resetPasswordsuccessfullyMessage) => {
+        ForgetPasswordPage.elements.successfullResetPasswordMessage().should("not.have.text",resetPasswordsuccessfullyMessage)
+    })
+
+
+})
+
+describe("US GX2-146 | TS GX2-147 | TC3: Validar que no permita recuperar la contraseña dejando el input “username” vacío", () => {
+    
+    Given("el usuario deja el input username vacio y hace click en el boton Reset Password", () => {
+        ForgetPasswordPage.elements.username_input().should("be.empty")
+        ForgetPasswordPage.elements.resetPassword_button().click()
+    })
+
+    Then("el border del input username se cambia a color rojo {string} y se muestra un mensaje {string}", (colorRojo,requiredMessage) => {
+        ForgetPasswordPage.elements.username_input().should("have.css", "border-color", colorRojo)
+        ForgetPasswordPage.elements.required_span().should("be.visible").and("have.text",requiredMessage)
+    })
+
+    And("No debe aparecer el mensaje {string}", (resetPasswordsuccessfullyMessage) => {
+    ForgetPasswordPage.elements.successfullResetPasswordMessage().should("not.have.text",resetPasswordsuccessfullyMessage)
+    })
+})
+
+//________________________________________________________________________
+// Comando predeterminado para que no ocurran errores de excepciones:
+Cypress.on('uncaught:exception', (err, runnable) => {
+	// returning false here prevents Cypress from
+	// failing the test
+	return false
+})
---
+++ cypress/support/pages/OrangeHRM/ForgetPasswordPage.js
@@ -0,0 +1,25 @@
+class ForgetPasswordPage {
+    constructor() {
+        this.url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode"
+    }
+
+    elements = {
+        "username_input": () => cy.get('input[name="username"]'),
+        "resetPassword_button": () => cy.get('button[type="submit"]'),
+        "cancel_button": () => cy.get('button[type="button"]'),
+        "required_span": () => cy.get('span'),
+        "successfullResetPasswordMessage": () => cy.get('.oxd-text--h6')
+    }
+
+    visit() {
+        cy.visit(this.url)
+    }
+
+    typeUsername(username) {
+        this.elements.username_input().type(username)
+    }
+
+}
+
+
+module.exports = new ForgetPasswordPage();

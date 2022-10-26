62d7998a29736cce456c09d696b85b717be3add8
Ely <elyermad@gmail.com>
2022-10-26 03:45:14 -0300

    cucumber example

--- cypress/e2e/cucumber-test/How-to/GherkinExample.feature
+++ cypress/e2e/cucumber-test/How-to/GherkinExample.feature
@@ -7,12 +7,26 @@ Feature: Labs: CI Framework Test for Xray with Cypress and GitHub Actions
 	#
 	#So that the Tests can be updated from Jira Action.
 
+	Background: Precondiciones para estar registrado
+		Given User is signed up
+		And User is at the login page
+
 	@TC_GX-4 @TS_GX-3 @BDD @Cucumber @Feature @Gherkin
 	Scenario Outline: US 2 | TS 3 | TC1: Check the Login correctly to Orange CRM Website
-		Given User is at the login page
-		When User enters username as '<username>' and password as '<password>'
-		And User clicks on login button
-		Then User is able to successfully login to the Website
+		When User enters username as '<username>' and password as '<password>' and clicks on login button
+		Then User should be able to login to the Website
+		And should display a friendly message as "User has successfully logged in"
 		Examples:
 			| username | password |
 			| Admin    | admin123 |
+
+	@TC_GX-4 @TS_GX-3 @BDD @Cucumber @Feature @Gherkin
+	Scenario Outline: US 2 | TS 3 | TC2: Check the Login is not possible when user enters invalid credentials
+		When User enters invalid username as '<username>' and password as '<password>' and clicks on login button
+		Then User should NOT be able to login to the Website
+		And should display an error message as "Please, insert a valid value"
+		Examples:
+			| username | password |
+			| Admin2    | admin1234 |
+	
+
--- cypress/e2e/cucumber-test/How-to/GherkinExample.js
+++ cypress/e2e/cucumber-test/How-to/GherkinExample.js
@@ -1,32 +1,49 @@
-import { Given, When, And, Then } from "@badeball/cypress-cucumber-preprocessor";
+import {Given, When, And, Then} from '@badeball/cypress-cucumber-preprocessor'
 
-describe("US 2 | TS 3 | TC1: Check the Login correctly to Orange CRM Website", () =>
-{
-    // (Given = Precondición) Dado que: el usuario está en la página de Login...
-    Given('User is at the login page', () =>
-    {
-        cy.visit('https://opensource-demo.orangehrmlive.com/')
-    })
-    // (When = Acción) Cuando: el usuario inserta username y password...
-    When('User enters username as {string} and password as {string}', (username, password) =>
-    {
-        cy.get('[placeholder="Username"]').type(username)
-        cy.get('[placeholder="Password"]').type(password)
-    })
-    // (And = Extensión) Y Cuando: el usuario hace click sobre el botón login...
-    And('User clicks on login button', () =>
-    {
-        cy.get('[type="submit"]').click()
-    })
-    // (Then = Extensión) Entonces: el usuario inicia sesión exitosamente en la Website.
-    Then('User is able to successfully login to the Website', () =>
-    {
-        cy.url().should('include',"viewEmployeeList")
-    })
+context('Feature: Login', () => {
+	// (Given = Precondición) Dado que: el usuario está en la página de Login...
+	Given('User is signed up', () => {
+		expect(1).eq(1)
+	})
+	And('User is at the login page', () => {
+		cy.visit('https://opensource-demo.orangehrmlive.com/')
+	})
+
+	describe('US 2 | TS 3 | TC1: Check the Login correctly to Orange CRM Website', () => {
+		// (When = Acción) Cuando: el usuario inserta un NombreDeUsuario como X y una Contraseña como X...
+		When('User enters username as {string} and password as {string} and clicks on login button', (username, password) => {
+			cy.get('[placeholder="Username"]').type(username)
+			cy.get('[placeholder="Password"]').type(password)
+			cy.get('[type="submit"]').click()
+		})
+		// (Then = Resultado Esperado) Entonces: el usuario DEBERÍA poder iniciar sesión en la Website.
+		Then('User should be able to login to the Website', () => {
+			cy.url().should('include', 'viewEmployeeList')
+		})
+		// (And = Extensión) Y Entonces: debería visualizarse un mensaje amigable como "bla bla bla"
+		And('should display a friendly message as {string}', (msg) => {
+			// Ejemplo de assertion usando parámetro del mismo Gherkin:
+			expect(msg).equal('User has successfully logged in')
+		})
+	})
+	describe('US 2 | TS 3 | TC2: Check the Login is not possible when user enters invalid credentials', () => {
+		// (When = Acción) Cuando: el usuario inserta un NombreDeUsuario como X y una Contraseña como X...
+		When('User enters invalid username as {string} and password as {string} and clicks on login button', (username, password) => {
+			cy.get('[placeholder="Username"]').type(username)
+			cy.get('[placeholder="Password"]').type(password)
+			cy.get('[type="submit"]').click()
+		})
+		// (Then = Resultado Esperado) Entonces: el usuario DEBERÍA poder iniciar sesión en la Website.
+		Then('User should NOT be able to login to the Website', () => {
+			cy.url().should('not.include', 'viewEmployeeList')
+		})
+		// (And = Extensión) Y Entonces: debería visualizarse un mensaje amigable como "bla bla bla"
+		And('should display an error message as {string}', (msg) => {
+			// Ejemplo de assertion usando parámetro del mismo Gherkin:
+			expect(msg).equal('Please, insert a valid value')
+		})
+	})
 })
-
-
-
 
 //________________________________________________________________________
 // Comando predeterminado para que no ocurran errores de excepciones:
@@ -38,7 +55,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 // Comando predeterminado para que no aparezcan los Fetch en el log del Test Runner:
 const origLog = Cypress.log
 Cypress.log = function (opts, ...other) {
-	if (opts.displayName === 'xhr'|| opts.displayName === 'fetch' && opts.url.startsWith('https://')) {
+	if (opts.displayName === 'xhr' || (opts.displayName === 'fetch' && opts.url.startsWith('https://'))) {
 		return
 	}
 	return origLog(opts, ...other)

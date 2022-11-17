ecf58d696c4531c158f5845167cfe1851e65cae1
KennyNavas <kenny.navas@gmail.com>
2022-11-17 19:44:41 -0400

    Actualización de US

--- cypress/e2e/test/Suites/GX2-260/recoveryPassword.cy.js
+++ cypress/e2e/test/Suites/GX2-260/recoveryPassword.cy.js
@@ -2,23 +2,29 @@ describe("✅OrangeHRM | Account | Recuperar contraseña olvidada por usuario",(
 
     beforeEach(()=>{
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode")
+        cy.url().should("contain","request")
     })
 
-    it("261 | TC1: Validar recuperación de contraseña exitosa",()=>{
+    it.only("261 | TC1: Validar poder recuperar contraseña correctamente",()=>{
         cy.get('[name="username"]').type("kenny")
+        cy.get("button[type='submit']").click()
+        cy.get("h6").contains("Reset Password link sent successfully").should("be.visible")
     })
 
-    it("261 | TC2: Validar no poder recuperar la contraseña cuando username es nulo",()=>{
-
+    it("261 | TC2: Validar NO poder recuperar la contraseña cuando el campo username esta vacío",()=>{
+        cy.get('[name="username"]').type("")
+        cy.get("button[type='submit']").click()
+        cy.get("h6").should("contain","Reset Password")
     })
 
-    it("261 | TC3: Validar no poder recuperar la contraseña cuando username no matchea",()=>{
-
+    it("261 | TC3: Validar NO poder recuperar la contraseña cuando el campo username no es válido",()=>{
+        cy.get('[name="username"]').type("kenny1$")
+        cy.get("button[type='submit']").click()
+        cy.url().should("not.contain", "requestPasswordResetCode")
     })
 
-    it("261 | TC4: Validar ir a pagina de log in con el botón cancelar",()=>{
-
+    it("261 | TC4: Validar poder ir a la pagina de login cuando se preciona el botón cancelar",()=>{
+        cy.get("button[type='button']").click()
+        cy.url().should("eql","https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     })
-
-    
 })

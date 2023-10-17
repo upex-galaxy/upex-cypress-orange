e90f9772e8feb3b00e1a1abf7f5ee40dea470b1f
ZwitterKaneda <smentek@gmail.com>
2023-10-17 14:59:50 -0300

    GX-38134 all test have passed :check:

--- cypress/e2e/Tests/Buzz/GX-38134-sharesLikeComments.cy.js
+++ cypress/e2e/Tests/Buzz/GX-38134-sharesLikeComments.cy.js
@@ -1,6 +1,8 @@
 import { buzzMenu } from '../../../support/pages/Buzz/GX-38134-sharesLikeComments';
 const userName = 'Admin';
 const password = 'admin123';
+import { faker } from '@faker-js/faker';
+const randomName = faker.name.firstName();
 describe('This is your test project title', () => {
 	beforeEach(() => {
 		cy.visit('/');
@@ -10,7 +12,7 @@ describe('This is your test project title', () => {
 		cy.url().should('contain', 'viewBuzz');
 	});
 
-	it.only('38135 | TC1: Validar la funcionalidad del botón Shares', () => {
+	it('38135 | TC1: Validar la funcionalidad del botón Shares', () => {
 		buzzMenu.clickSharedButton('post');
 		buzzMenu.get.postSuccess().should('have.length', 1).and('be.visible');
 		buzzMenu.clickSharedButton('cancel');
@@ -21,7 +23,15 @@ describe('This is your test project title', () => {
 		buzzMenu.get.likeButton().should('have.class', 'orangehrm-like-animation');
 	});
 	it('38135 | TC3: Validar la funcionalidad del botón Comment...', () => {
-		expect(1).eq(1);
+		buzzMenu.clickCommentButtonAndType(randomName);
+		buzzMenu.get.containerBoxInput().then($box => {
+			if ($box.length === 4) {
+				buzzMenu.get.showMoreText().click({ force: true });
+				buzzMenu.get.commentBox().should('have.text', randomName);
+			} else {
+				buzzMenu.get.commentBox().should('have.text', randomName);
+			}
+		});
 	});
 });
 import { removeLogs } from '@helper/RemoveLogs';
--- cypress/support/pages/Buzz/GX-38134-sharesLikeComments.js
+++ cypress/support/pages/Buzz/GX-38134-sharesLikeComments.js
@@ -8,9 +8,13 @@ class SLC {
 
 		likeButton: () => cy.get('[class="orangehrm-buzz-post-actions"] div').first(), //esto obtiene todo el array de likes de la página
 		commentButton: () => cy.get('div[class="orangehrm-buzz-post-actions"] button[class="oxd-icon-button"]').first(), //esto obtiene todo el array de comments de la página
+		commentInput: () => cy.get('form input', { timeout: 3000 }),
+		commentBox: () => cy.get('div[class="orangehrm-post-comment"] span.orangehrm-post-comment-text').last(),
 		shareButton: () => cy.get('div[class="orangehrm-buzz-post-actions"] button[class="oxd-icon-button"]').last(), //esto obtiene todo el array de shares de la página
 		popupComment: () => cy.get('form[class="oxd-form"]').last(),
 		postSuccess: () => cy.get('[id="oxd-toaster_1"] [class*="oxd-toast--success"]', { timeout: 2000 }),
+		showMoreText: () => cy.get('[class="orangehrm-buzz-comment"] p[class$="buzz-comment-readmore"]', { timeout: 2000 }),
+		containerBoxInput: () => cy.get('[class="orangehrm-buzz-comment"] [class$="orangehrm-post-comment-text"]'),
 	};
 	fillLoginAndSubmit(userName, password) {
 		this.get.userNameInput().type(userName);
@@ -41,6 +45,20 @@ class SLC {
 					cy.get('button').first().should('be.visible').click().end();
 				}
 			});
+	}
+	clickCommentButtonAndType(comment) {
+		this.get
+			.postContainer()
+			.first()
+			.within(() => {
+				this.get.commentButton().click({ force: true });
+			});
+		this.get.commentInput().should('have.class', 'oxd-input--focus').type(`${comment}{enter}`, { force: true });
+	}
+	showMoreBttn() {
+		return buzzMenu.get.showMoreText().then(showMore => {
+			return showMore.length;
+		});
 	}
 }
 export const buzzMenu = new SLC();

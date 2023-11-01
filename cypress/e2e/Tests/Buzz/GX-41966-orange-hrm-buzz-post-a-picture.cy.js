69f5123f30ded4be83b8bbc7491b1f2b31cd8861
rafo18 <rafaelbarreto10@hotmail.com>
2023-11-01 19:10:19 -0400

    GX-41966 Se esta realizando el primer TC

--- cypress/e2e/Tests/Buzz/GX-41966-orange-hrm-buzz-post-a-picture.cy.js
+++ cypress/e2e/Tests/Buzz/GX-41966-orange-hrm-buzz-post-a-picture.cy.js
@@ -1,5 +1,31 @@
-import { buszzPage } from "@pages/Buzz/GX-41966-postAPicture";
+import { buzzPage } from '@pages/Buzz/GX-41966-postAPicture';
+import { loginPage } from '@pages/Buzz/GX-41966-postAPicture';
+const login = Cypress.env('AdminUser');
 
 describe('GX-41966-orange-hrm-buzz-post-a-picture)ge-hrm-buzz-post-a-picture', () => {
-    
+	beforeEach(() => {
+		cy.visit('/');
+		cy.url().should('contain', 'login');
+		loginPage.login({
+			username: login.username,
+			password: login.password,
+		});
+		cy.url().should('contain', 'dashboard');
+
+		cy.visit('/buzz/viewBuzz');
+		cy.url().should('contain', 'viewBuzz');
+	});
+	it('41967 | TC1: Validar compartir una imagen en la feed.', () => {
+		cy.wait(4000);
+		buzzPage.get.sharePhotoButton().click();
+		buzzPage.get
+			.postPopUp()
+			.should('exist')
+			.within(() => {
+				buzzPage.get.sharephotobutton().click();
+				buzzPage.get.addPhotos().invoke('removeClass', 'oxd-file-input').attachFile('images/upexlogo.png');
+				// buzzPage.get.shareButton().invoke('removeAttr', 'disabled').click({ force: true });
+			});
+		// cy.wait(4000);
+	});
 });
--- cypress/support/pages/Buzz/GX-41966-postAPicture.js
+++ cypress/support/pages/Buzz/GX-41966-postAPicture.js
@@ -1,5 +1,27 @@
-class BuzzPage {
+class LoginPage {
+	get = {
+		username: () => cy.get('[name=username]'),
+		password: () => cy.get('[name=password]'),
+		loginButton: () => cy.get('[class^=oxd-button]'),
+	};
 
+	login({ username, password }) {
+		this.get.username().clear().should('be.empty').type(username);
+		this.get.password().clear().should('be.empty').type(password);
+		this.get.loginButton().click();
+	}
 }
 
-export const buszzPage = new BuzzPage()
+class BuzzPage {
+	get = {
+		sharePhotoButton: () => cy.get('[class = oxd-glass-button]').eq(0),
+		postPopUp: () => cy.get('[role=document]'),
+		addPhotos: () => cy.get('input[type=file]'),
+		addPhotos2: () => cy.get('[class=orangehrm-photo-input-field]'),
+		sharephotobutton: () => cy.get('[class^=oxd-file-div]'),
+		shareButton: () => cy.get('button[type=submit]'),
+	};
+}
+
+export const buzzPage = new BuzzPage();
+export const loginPage = new LoginPage();

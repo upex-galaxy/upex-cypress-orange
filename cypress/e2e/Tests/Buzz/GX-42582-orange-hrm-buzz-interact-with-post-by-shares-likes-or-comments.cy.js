44910e39810104254f7277d2194319d115ad804d
rafo18 <rafaelbarreto10@hotmail.com>
2023-11-07 14:56:15 -0400

    GX-42582 Se realizo el segundo test case

--- cypress/e2e/Tests/Buzz/GX-42582-orange-hrm-buzz-interact-with-post-by-shares-likes-or-comments.cy.js
+++ cypress/e2e/Tests/Buzz/GX-42582-orange-hrm-buzz-interact-with-post-by-shares-likes-or-comments.cy.js
@@ -19,10 +19,22 @@ describe('GX-42582-orange-hrm-buzz-interact-with-post-by-shares-likes-or-comment
 	});
 
 	it('42583 | TC1: Validar la funcionalidad del boton share.', () => {
-		buzzPage.sharePost({
+		buzzPage.shareAPost({
 			comment: randomComment,
 		});
 		buzzPage.get.postSucces().should('exist').and('have.text', 'SuccessSuccessfully Saved');
 		buzzPage.get.postContainer().should('contain.text', randomComment);
+	});
+	it('42583 | TC2: Validar la funcionalidad del boton like.', () => {
+		buzzPage.likeAPost().then(number => {
+			buzzPage.get.postFooter().within(() => {
+				if (number === 0) {
+					buzzPage.get.textLikes().should('have.text', `${number + 1} Like`);
+				} else {
+					buzzPage.get.textLikes().should('have.text', `${number + 1} Likes`);
+				}
+			});
+		});
+		buzzPage.get.likeButton().first().should('have.class', 'orangehrm-like-animation');
 	});
 });
--- cypress/support/pages/Buzz/GX-42582interactWithPost.page.js
+++ cypress/support/pages/Buzz/GX-42582interactWithPost.page.js
@@ -18,17 +18,37 @@ class BuzzPage {
 		popUpShareButton: () => cy.get('button[type=submit]').eq(1),
 		postSucces: () => cy.get('div[class^=oxd-toast-content]', { timeout: 15000 }),
 		postContainer: () => cy.get('[class ^= oxd-sheet]').eq(1),
+		postFooter: () => cy.get('[class=orangehrm-buzz-post-footer]').first(),
 		// alertContent: () => cy.get('[class^=oxd-alert-content]'),
 		shareButton: () => cy.get('[class$=bi-share-fill]').first(),
+		likeButton: () => cy.get('[class="orangehrm-buzz-post-actions"] div'),
+		textLikes: () => cy.get('[class^=oxd-text]').first(),
 		commentInput: () => cy.get('[class = oxd-buzz-post-input]').eq(1),
 	};
 
-	sharePost({ comment }) {
+	shareAPost({ comment }) {
 		cy.wait(4000);
 		this.get.shareButton().click({ force: true });
 		this.get.postPopUp().should('exist');
 		this.get.commentInput().clear().should('be.empty').type(comment);
 		this.get.popUpShareButton().click();
+	}
+
+	likeAPost() {
+		let likeText;
+		return cy
+			.get('*')
+			.then(() => {
+				this.get.postFooter().within(() => {
+					this.get.textLikes().then($text => {
+						likeText = parseInt($text.text().slice(0, 1));
+					});
+					this.get.likeButton().click({ force: true });
+				});
+			})
+			.then(() => {
+				return likeText;
+			});
 	}
 }
 

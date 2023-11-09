53988705f85db0b1ded68add98a41ffd22bed4e1
rafo18 <rafaelbarreto10@hotmail.com>
2023-11-09 00:15:42 -0400

    GX-42582 Se esta realizando el tercer TC

--- cypress/e2e/Tests/Buzz/GX-42582-orange-hrm-buzz-interact-with-post-by-shares-likes-or-comments.cy.js
+++ cypress/e2e/Tests/Buzz/GX-42582-orange-hrm-buzz-interact-with-post-by-shares-likes-or-comments.cy.js
@@ -26,15 +26,22 @@ describe('GX-42582-orange-hrm-buzz-interact-with-post-by-shares-likes-or-comment
 		buzzPage.get.postContainer().should('contain.text', randomComment);
 	});
 	it('42583 | TC2: Validar la funcionalidad del boton like.', () => {
-		buzzPage.likeAPost().then(number => {
+		buzzPage.likeAPost().then(numberOfLikes => {
 			buzzPage.get.postFooter().within(() => {
-				if (number === 0) {
-					buzzPage.get.textLikes().should('have.text', `${number + 1} Like`);
+				if (numberOfLikes === 0) {
+					buzzPage.get.textLikes().should('have.text', `${numberOfLikes + 1} Like`);
 				} else {
-					buzzPage.get.textLikes().should('have.text', `${number + 1} Likes`);
+					buzzPage.get.textLikes().should('have.text', `${numberOfLikes + 1} Likes`);
 				}
 			});
 		});
 		buzzPage.get.likeButton().first().should('have.class', 'orangehrm-like-animation');
+	});
+	it.only('42583 | TC3: Validar hacer un comentario en un post de la feed.', () => {
+		buzzPage.comentAPost({ comment: randomComment });
+		buzzPage.get.commentsContainer().then(data => {
+			console.log(data);
+			console.log(data.length);
+		});
 	});
 });

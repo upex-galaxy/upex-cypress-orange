1fe3c08dfac47fe1e79cd571390db09209a4c26c
igflores <139146133+igflores@users.noreply.github.com>
2024-02-17 18:28:56 -0600

    test(GX3-2356): Addeed TC01: Should turn red the like button when clicking on it, pending do it random

--- cypress/e2e/Tests/Buzz/GX3-2356/GX3-2356-interact_with_post_by_shares_likes_or_comments-Isa.cy.js
+++ cypress/e2e/Tests/Buzz/GX3-2356/GX3-2356-interact_with_post_by_shares_likes_or_comments-Isa.cy.js
@@ -13,7 +13,9 @@ describe('GX3-2356| OrangeHRM | Buzz | Interact with post by Shares, Likes or Co
 	});
 
 	it('GX3-2361 | TC01: Should turn red the like button when clicking on it', () => {
-		expect(1).equal(1);
+		// const givenPost = buzzPage.getAnyPost();
+		buzzPage.clickLikeButton();
+		buzzPage.get.likeButtons().should('have.class', 'orangehrm-like-animation');
 	});
 });
 
--- cypress/support/pages/Buzz/GX3-2356-interact_with_post_shares_likes_comments-isa.js
+++ cypress/support/pages/Buzz/GX3-2356-interact_with_post_shares_likes_comments-isa.js
@@ -2,7 +2,8 @@ class BuzzPage {
 	get = {
 		selectMenuButtton: menuName => cy.get(`[class="oxd-main-menu-item-wrapper"] [href*="${menuName}"]`),
 		postItem: () => cy.get('[class*="oxd-sheet--white orangehrm-buzz"]:not([class*="gutters"])'),
-		likeButtons: () => cy.get('.orangehrm-buzz-post-actions div svg'),
+		likeButtons: () => cy.get('[class="orangehrm-buzz-post-actions"] div').first(),
+		// isRedHeartVisible: () => cy.get('.orangehrm-like-animation'),
 		commentButtons: () => cy.get('.orangehrm-buzz-post-actions button [class$="bi-chat-text-fill"]'),
 		shareButtons: () => cy.get('.orangehrm-buzz-post-actions button [class$="bi-share-fill"]'),
 		shareButtonPopUp: () => cy.get('[class$="orangehrm-buzz-post-modal-actions"] button'),
@@ -16,6 +17,10 @@ class BuzzPage {
 
 	goToBuzzPage(nameMenu) {
 		this.get.selectMenuButtton(nameMenu).click();
+	}
+
+	clickLikeButton() {
+		this.get.likeButtons().click({ force: true });
 	}
 }
 

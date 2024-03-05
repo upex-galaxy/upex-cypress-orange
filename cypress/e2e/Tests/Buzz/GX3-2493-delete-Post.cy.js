3486bda62ac306d8eee918b09a47993ea5d180a2
AndreaDev14 <andreatester17@gmail.com>
2024-03-05 14:19:44 -0300

    GX3-2493: Added 3TCS and 2 pages file

---
+++ cypress/e2e/Tests/Buzz/GX3-2493-delete-Post.cy.js
@@ -0,0 +1,40 @@
+import { login } from '@pages/Buzz/GX3-2493-login.Page';
+import { post } from '@pages/Buzz/GX3-2493.post.Page';
+const { username, password } = Cypress.env('AdminUser');
+import { removeLogs } from '@helper/RemoveLogs';
+removeLogs();
+
+describe('GX3-2493 | [Automation] OrangeHRM | Buzz | Delete a post', () => {
+	beforeEach(() => {
+		cy.session('login', () => {
+			cy.visit('/');
+			cy.url().should('contain', 'orangehrmlive');
+			login.validLogin(username, password);
+		});
+		cy.visit('/buzz/viewBuzz');
+		cy.url().should('contain', 'viewBuzz');
+	});
+
+	it('GX3-2493 | TC1: Validate  admin tries to delete a post', () => {
+		post.selectRandomPostAndClickDeleteLink();
+
+		post.verifyPopUpIsShown();
+		post.verifyDeleteButtonIsVisible();
+		post.verifyCancelButtonIsVisible();
+	});
+
+	it('GX3-2493 |TC2: Validate delete a post', () => {
+		post.selectRandomPostAndClickDeleteLink();
+
+		post.verifyPopUpIsShown();
+		post.clickDeleteButton();
+		post.verifySuccessMessageIsDisplayed('Successfully Deleted');
+		post.verifyPostNotVisible();
+	});
+
+	it('GX3-2493 |TC3: Validate cancel deleting a post', () => {
+		post.selectRandomPostAndClickDeleteLink();
+		post.clickCancelButton();
+		post.verifyPostIsVisible();
+	});
+});

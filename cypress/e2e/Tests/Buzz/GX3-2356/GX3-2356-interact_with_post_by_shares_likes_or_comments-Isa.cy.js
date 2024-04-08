28a8beec24c5dd9bed616a544501e762a0b2606a
igflores <139146133+igflores@users.noreply.github.com>
2024-04-08 10:55:22 -0500

    test(test/GX3-2356/interact_with_post_by_shares_likes_or_comments): again

--- cypress/e2e/Tests/Buzz/GX3-2356/GX3-2356-interact_with_post_by_shares_likes_or_comments-Isa.cy.js
+++ cypress/e2e/Tests/Buzz/GX3-2356/GX3-2356-interact_with_post_by_shares_likes_or_comments-Isa.cy.js
@@ -1,6 +1,7 @@
 import { removeLogs } from '@helper/RemoveLogs';
 import { orangeLoginPage } from '@pages/orangeHRMLoginPage';
 import { buzzPage } from '@pages/Buzz/GX3-2356-interact_with_post_shares_likes_comments-isa';
+import { faker } from '@faker-js/faker';
 
 describe('GX3-2356| OrangeHRM | Buzz | Interact with post by Shares, Likes or Comments', () => {
 	const { username: usedUsername, password: usedPassword } = Cypress.env('AdminUser');
@@ -17,6 +18,22 @@ describe('GX3-2356| OrangeHRM | Buzz | Interact with post by Shares, Likes or Co
 		buzzPage.get.likeButtons().should('have.class', 'orangehrm-like-animation');
 		buzzPage.get.isRedHeartVisible().should('be.visible');
 	});
+
+	it.skip('GX3-2361 | TC02: Should can comment successfully in a post when the Enter key is pressed in the textbox', () => {
+		const randomComment = faker.lorem.sentence();
+		buzzPage.writeYourComment({ comment: randomComment });
+		buzzPage.get.commentInput().should('have.value', randomComment);
+		buzzPage.get.commentWrapper().contains(randomComment).should('be.visible');
+
+		// buzzPage.get.commentWrapper().then($commentWrapper => {
+		// 	if ($commentWrapper.find(buzzPage.get.showMorePosts().length !== 0)) {
+		// 		buzzPage.get.showMorePosts().click({ force: true });
+		// 	}
+		// 	buzzPage.get.commentInput().should('contain', randomComment);
+		// });
+	});
+
+	it.skip('GX3-2361 | TC03: Should display a post when the "Share" button in the pop-up is clicked', () => {});
 });
 
 removeLogs();
--- cypress/support/pages/Buzz/GX3-2356-interact_with_post_shares_likes_comments-isa.js
+++ cypress/support/pages/Buzz/GX3-2356-interact_with_post_shares_likes_comments-isa.js
@@ -1,10 +1,15 @@
 class BuzzPage {
 	get = {
 		selectMenuButtton: menuName => cy.get(`[class="oxd-main-menu-item-wrapper"] [href*="${menuName}"]`),
-		postItem: () => cy.get('[class*="oxd-sheet--white orangehrm-buzz"]:not([class*="gutters"])'),
+		postItem: () => cy.get('[class*="oxd-sheet--white orangehrm-buzz"]:not([class*="gutters"])').eq(0),
 		likeButtons: () => cy.get('[class="orangehrm-buzz-post-actions"] div').first(),
 		isRedHeartVisible: () => cy.get('.orangehrm-like-animation'),
-		commentButtons: () => cy.get('.orangehrm-buzz-post-actions button [class$="bi-chat-text-fill"]'),
+
+		commentButtons: () => cy.get('div [class=orangehrm-buzz-post-actions] button [class$="bi-chat-text-fill"]').first(),
+		commentInput: () => cy.get('[class="orangehrm-buzz-comment-add"] form input').first(),
+		commentWrapper: () => cy.get('[class=orangehrm-buzz-comment]'),
+		showMorePosts: () => cy.get('[class$=buzz-comment-readmore]'),
+
 		shareButtons: () => cy.get('.orangehrm-buzz-post-actions button [class$="bi-share-fill"]'),
 		shareButtonPopUp: () => cy.get('[class$="orangehrm-buzz-post-modal-actions"] button'),
 		sharePopUp: () =>
@@ -12,7 +17,6 @@ class BuzzPage {
 				'[class$="oxd-sheet--rounded oxd-sheet--white oxd-dialog-sheet oxd-dialog-sheet--shadow oxd-dialog-sheet--gutters orangehrm-dialog-modal"]'
 			),
 		shareSuccessfulMessage: () => cy.get('[class*="oxd-text--toast-message"]'),
-		writeCommentInput: () => cy.get('[class="orangehrm-buzz-comment-add"] form input'),
 	};
 
 	goToBuzzPage(nameMenu) {
@@ -22,6 +26,23 @@ class BuzzPage {
 	clickLikeButton() {
 		this.get.likeButtons().click({ force: true });
 	}
-}
 
+	writeYourComment({ comment }) {
+		this.get.postItem().within(() => {
+			this.get.commentButtons().click({ force: true });
+			this.get.commentInput().clear().should('be.empty').type(`${comment}{enter}`);
+			cy.wait(3000);
+		});
+	}
+
+	// clickShareButton() {
+	// 	const listShareButtons = this.get.shareButtons();
+	// 	listShareButtons.click();
+	// }
+
+	// clickshareButtonPopUp() {
+	// 	const shareButtonPopUP = this.get.shareButtonPopUp();
+	// 	shareButtonPopUP.click();
+	// }
+}
 export const buzzPage = new BuzzPage();

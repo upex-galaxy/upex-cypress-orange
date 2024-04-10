2686a55d5eb6162b5351f696ad5bf4acace79ab6
igflores <139146133+igflores@users.noreply.github.com>
2024-04-10 11:31:06 -0500

    test(test/GX3-3039/Buscar_un_usuario): Added preconditions, using Cypress environment variables, baseUrl, and the Login command.

--- cypress/e2e/Tests/Admin/GX3-3039-findUser.cy.js
+++ cypress/e2e/Tests/Admin/GX3-3039-findUser.cy.js
@@ -1,1 +1,16 @@
-//starting
+const { username, password } = Cypress.env('AdminUser');
+
+describe('GX3-3039 | OrangeHRM | Admin | Buscar un usuario', () => {
+	beforeEach('Precondition', () => {
+		cy.visit('/');
+		cy.url().should('contain', 'login');
+
+		cy.Login(username, password);
+		cy.visit('/admin/viewSystemUsers');
+		cy.contains('User Management').should('be.visible');
+	});
+
+	it('', () => {
+		expect(1).equal(1);
+	});
+});

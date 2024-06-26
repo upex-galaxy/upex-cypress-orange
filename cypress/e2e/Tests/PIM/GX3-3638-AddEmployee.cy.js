1d20dfa5061759895a4b49c9d34c396cf1aa67a1
jhoannaf <jhoannafrancia@gmail.com>
2024-06-26 17:37:39 -0300

    another change

--- cypress/e2e/Tests/PIM/GX3-3638-AddEmployee.cy.js
+++ cypress/e2e/Tests/PIM/GX3-3638-AddEmployee.cy.js
@@ -1,6 +1,6 @@
 import { Login } from '@pages/Login.Page.js';
 import { PimPage } from '@pages/Buzz/PIM.page.js';
-import  json   from 'cypress/fixtures/data/registreUser.json';
+import json from 'cypress/fixtures/data/registreUser.json';
 describe('US GX3-3638 | TS: ✅OrangeHRM | PIM | Agregar un nuevo empleado con usuario', () => {
 	beforeEach('Login to OrangeHRM', () => {
 		cy.visit('/auth/login');
@@ -10,7 +10,6 @@ describe('US GX3-3638 | TS: ✅OrangeHRM | PIM | Agregar un nuevo empleado con u
 		cy.url().should('include', 'dashboard');
 	});
 	it('3638 | TC1: Validate to add new a employee profile to the HR without credentials successfully.', () => {
-		
-		PimPage.typeFirstName(json.);
+		PimPage.typeFirstName();
 	});
 });
